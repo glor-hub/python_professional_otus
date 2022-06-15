f0e46fb1b2633aa3f942c15cee452b1e7eb3fdf4
Inna Sorina <sorina-inna@yandex.ru>
2022-06-15 17:32:35 +0300

    add configuration parse.

--- homework1/log_analyzer.py
+++ homework1/log_analyzer.py
@@ -7,15 +7,52 @@
 #                     '"$http_user_agent" "$http_x_forwarded_for" "$http_X_REQUEST_ID" "$http_X_RB_USER" '
 #                     '$request_time';
 
-config = {
+import os
+import argparse
+import configparser
+
+default_config = {
     "REPORT_SIZE": 1000,
     "REPORT_DIR": "./reports",
     "LOG_DIR": "./log"
 }
 
+DEFAULT_CONFIG_FILE_PATH = os.path.abspath('config.ini')
+
+
+def get_config_path():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--config',
+        default=DEFAULT_CONFIG_FILE_PATH,
+        help='Path to configuration file'
+    )
+    args = parser.parse_args()
+    config_path = args.config
+    print("config_path:", config_path)
+    return config_path
+
+
+def parse_config(def_config, config_path):
+    parser = configparser.ConfigParser()
+    parser.read(config_path)
+    config = def_config.copy()
+
+    if not os.path.isfile(config_path):
+        raise Exception("Config file not found")
+    try:
+        for key in parser["log_analyzer"].keys():
+            config[key.upper()] = parser["log_analyzer"][key]
+        return config
+    except configparser.Error:
+        raise
 
 def main():
-    pass
+    print('hello')
+    config_path = get_config_path()
+    config=parse_config(default_config, config_path)
+    print(config)
+
 
 if __name__ == "__main__":
     main()
