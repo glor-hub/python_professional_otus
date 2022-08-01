20e8ef41e21840a7fe56f5173491a7d0c71b7c60
Inna Sorina <sorina-inna@yandex.ru>
2022-08-01 13:25:30 +0300

    update test_store.py

--- homework4/tests/test_store.py
+++ homework4/tests/test_store.py
@@ -9,7 +9,6 @@ class TestStoreOnConnectSuccess(unittest.TestCase):
     def setUp(self):
         self.redis_storage = store.RedisStorage()
         self.redis_storage.connect()
-        self.redis_storage.db.connected = True
         self.store = store.Store(self.redis_storage,3)
 
     @patch("redis.StrictRedis", fakeredis.FakeStrictRedis)
@@ -28,7 +27,28 @@ class TestStoreOnConnectSuccess(unittest.TestCase):
         self.assertEqual(self.store.cache_get("foo"), "bar")
         self.assertEqual(self.store.get("foo"), "bar")
 
+class TestStoreOnConnectError(unittest.TestCase):
+    def setUp(self):
+        server = fakeredis.FakeServer()
+        server.connected = False
+        self.redis_storage = store.RedisStorage()
+        self.redis_storage.db=fakeredis.FakeStrictRedis(server=server)
+        self.store = store.Store(self.redis_storage,1)
 
+    def tearDown(self):
+        self.redis_storage.db.close()
+
+    def test_raises_set_and_get_data_to_db(self):
+        with self.assertRaises(ConnectionError):
+            self.redis_storage.set("key1","value1",600)
+        with self.assertRaises(ConnectionError):
+            self.redis_storage.get("key1")
+
+    def test_failed_set_and_get_cache_data_to_store(self):
+
+        self.assertEqual(self.store.cache_set("foo3", "bar3",600), None)
+        self.assertEqual(self.store.cache_get("foo3"), None)
+        self.assertEqual(self.store.get("foo3"), None)
 
 if __name__ == "__main__":
     unittest.main()
