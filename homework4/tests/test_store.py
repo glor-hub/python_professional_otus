d9d9f48c6adb8f261209e9aed25542a8c6b4ef03
Inna Sorina <sorina-inna@yandex.ru>
2022-08-01 10:28:01 +0300

    update test_store.py

--- homework4/tests/test_store.py
+++ homework4/tests/test_store.py
@@ -11,18 +11,21 @@ class TestStoreOnConnectSuccess(unittest.TestCase):
         self.redis_storage.db.connected = True
         self.store = store.Store(self.redis_storage,3)
 
+    def tearDown(self):
+        self.redis_storage.db.close()
+
     @patch("redis.StrictRedis", fakeredis.FakeStrictRedis)
     def test_set_valid_data_to_db(self):
-        self.assertEqual(self.redis_storage.db.set("key","value"), True)
+        self.assertEqual(self.redis_storage.set("key","value",600), True)
 
     @patch("redis.StrictRedis", fakeredis.FakeStrictRedis)
     def test_get_valid_data_from_db(self):
-        self.assertEqual(self.redis_storage.db.get("key"), "value")
+        self.assertEqual(self.redis_storage.get("key"), "value")
 
     @patch("redis.StrictRedis", fakeredis.FakeStrictRedis)
     def test_get_non_exist_data_from_db(self):
-        self.assertNotEqual(self.redis_storage.db.get("ke"), "value")
-        self.assertEqual(self.redis_storage.db.get("ke"), None)
+        self.assertNotEqual(self.redis_storage.get("ke"), "value")
+        self.assertEqual(self.redis_storage.get("ke"), None)
 
 
     @patch("redis.StrictRedis", fakeredis.FakeStrictRedis)
@@ -31,7 +34,7 @@ class TestStoreOnConnectSuccess(unittest.TestCase):
 
     @patch("redis.StrictRedis", fakeredis.FakeStrictRedis)
     def test_set_cache_data_to_store(self):
-        self.assertEqual(self.store.cache_set("foo", "bar",60), True)
+        self.assertEqual(self.store.cache_set("foo", "bar",600), True)
 
     @patch("redis.StrictRedis", fakeredis.FakeStrictRedis)
     def test_get_cache_data_from_store(self):
