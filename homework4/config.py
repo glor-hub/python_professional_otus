01d47a6806d5d1f5640f4fece3dfd1a90a1cf940
Inna Sorina <sig@vpribor.spb.ru>
2022-08-16 18:20:27 +0300

    small fix.

--- homework4/config.py
+++ homework4/config.py
@@ -5,5 +5,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 REDIS_HOST = os.getenv('REDIS_HOST')
-REDIS_PORT = os.getenv('REDIS_PORT')
-REDIS_TIMEOUT = os.getenv('REDIS_TIMEOUT')
+REDIS_PORT = int(os.getenv('REDIS_PORT'))
+REDIS_TIMEOUT = int(os.getenv('REDIS_TIMEOUT'))
