190d36d83541b89482d3a6c00d14543d6b959ebf
Inna Sorina <sig@vpribor.spb.ru>
2022-12-02 14:20:21 +0300

    improve getting success_url.

--- homework7_8/hasker/accounts/views.py
+++ homework7_8/hasker/accounts/views.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from django.views.generic import CreateView, UpdateView
 
 from .mixins import AccountActionMixin
@@ -10,15 +11,18 @@ from .models import Account
 
 class AccountCreateView(CreateView):
     model = Account
-    success_url = '/hasker/'
     form_class = AccountCreateForm
+
+    def get_success_url(self):
+        return reverse('main:index')
 
 
 
 class AccountUpdateView(AccountActionMixin,
                          UpdateView):
     model = Account
-    success_url = '/hasker/'
     success_msg = "User`s data updated!"
     form_class = AccountUpdateForm
 
+    def get_success_url(self):
+        return reverse('main:index')
