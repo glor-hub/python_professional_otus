91607c4713c8579a4016e3cd39e656345f07da25
Inna Sorina <sig@vpribor.spb.ru>
2022-12-02 13:18:45 +0300

    add set_avatar().

--- homework7_8/hasker/accounts/models.py
+++ homework7_8/hasker/accounts/models.py
@@ -1,9 +1,17 @@
+import os
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+from hasker import settings
+
 
 class Account(AbstractUser):
     date_joined = models.DateTimeField(default=timezone.now)
-    avatar = models.ImageField(upload_to='accounts/', null=True)
+    avatar = models.ImageField(upload_to='accounts/avatars/', null=True)
+    def get_avatar(self):
+       if not self.avatar:
+           return os.path.join(settings.STATIC_URL, "default_avatar.png")
+       else:
+           return os.path.join(settings.MEDIA_URL, self.avatar.name)
