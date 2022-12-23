9e9598a302dff2043cbd170f35b7b1401ef47c59
Inna Sorina <sig@vpribor.spb.ru>
2022-12-23 13:34:22 +0300

    added more readable view for foreign key in serializers.

--- homework9/hasker/main/api/serializers.py
+++ homework9/hasker/main/api/serializers.py
@@ -3,11 +3,27 @@ from main.models import Answer,Question
 
 
 class AnswerSerializer(serializers.ModelSerializer):
+
+    user=serializers.CharField(source='user.username')
+    question = serializers.CharField(source='question.title')
+
     class Meta:
         model = Answer
+        # fields=[
+        #     'body',
+        #     'create_at',
+        #     'votes_total',
+        # 'rating',
+        # 'votes_like',
+        # 'votes_dislike',
+        # 'is_favorite',
+        # 'user',
+        # 'question']
         fields='__all__'
 
 class QuestionSerializer(serializers.ModelSerializer):
+    author = serializers.CharField(source='author.username')
+
     class Meta:
         model = Question
         fields='__all__'
